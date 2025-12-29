0e9b5707a4cc793bc88cf279d24dde3a840172da
anshikarana806-hue <anshikarana806@gmail.com>
2025-12-29 07:18:30 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 from image_detector import analyze_image
 from video_detector import analyze_video
 
 app = FastAPI(title="AI Generated Media Detector")
 
-@app.get("/")
-def root():
-    return {"status": "AI Detector API running"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/detect/image")
 async def detect_image(file: UploadFile = File(...)):
-    result = analyze_image(file.file)
-    return result
+    return analyze_image(file.file)
 
 @app.post("/detect/video")
 async def detect_video(file: UploadFile = File(...)):
-    result = analyze_video(file.file)
-    return result
+    return analyze_video(file.file)
